87178570912247f96ff5d61687dd089e145f8e17
Colin Lancaster <clancaster@lendingclub.com>
2015-07-29 15:00:31 -0700

    Scheduler at work

--- chp10/processes.erl
+++ chp10/processes.erl
@@ -1,8 +1,15 @@
 -module(processes).
 -compile(export_all).
 
+many_processes() ->
+    % demonstrates that the processes will exit in a psuedo random order based on scheduler
+    G = fun(X) -> timer:sleep(100), io:format("~p~n", [X]) end,
+    [spawn(fun() -> G(X) end) || X <- lists:seq(1, 100)].
+
 main() ->
     F = fun() -> io:format("In a function!~n") end,
     spawn(F),
     io:format("Hello world!~n"),
+    many_processes(),
+    timer:sleep(5000),
     erlang:halt(0).
