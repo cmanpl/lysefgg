2e0ecd24c4d2f4a0c1ee08dc617696da3ccf9f3c
Colin Lancaster <clancaster@lendingclub.com>
2015-08-03 11:04:15 -0700

    Finishing up the rest of the dolphin examples

--- chp10/dolphins.erl
+++ chp10/dolphins.erl
@@ -11,8 +11,46 @@ dolphin1() ->
             io:format("Heh, we're smarter than humans.~n")
     end.
 
+dolphin2() ->
+    receive
+        {From, do_a_flip} ->
+            From ! "How about no?";
+        {From, fish} ->
+            From ! "So long and thanks for the fish!";
+        _ ->
+            io:format("Heh, we're smarter than humans.~n")
+    end.
+
+dolphin3() ->
+    receive
+        {From, do_a_flip} ->
+            From ! "How about no?",
+            dolphin3();
+        {From, fish} ->
+            From ! "So long and thanks for all the fish!";
+        _ ->
+            io:format("Heh, we're smarter than humans.~n"),
+            dolphin3()
+    end.
+
+print_message() ->
+    receive
+        Message ->
+            io:format("Message was: ~s~n", [Message])
+    end.
+
 main() ->
     Dolphin = spawn(dolphins, dolphin1, []),
     Dolphin ! "Oh! Hello dolphin",
-    timer:sleep(1000),
+    Dolphin2 = spawn(dolphins, dolphin2, []),
+    Dolphin2 ! {self(), fish},
+    print_message(),
+    Dolphin3 = spawn(dolphins, dolphin3, []),
+    Dolphin3 ! {self(), do_a_flip},
+    Dolphin3 ! {self(), do_a_flip},
+    Dolphin3 ! {self(), unknown_command},
+    print_message(),
+    print_message(),
+    Dolphin3 ! {self(), fish},
+    print_message(),
     erlang:halt().
